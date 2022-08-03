8102399634f564000484e16b316e67e26503db2e
Shapikk <97690223+Shapikk@users.noreply.github.com>
2022-08-03 12:40:22 +0200

    commit

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,14 @@
 
-const NavEl = document.querySelector ("#categories");
+const navEl = document.querySelectorAll(".item");
+
+const numberOfCategories = navEl.length;
+
+navEl.forEach((element) => {
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Element: ${element.lastElementChild.children.length}`);
+});
+
+console.log(`Number og categoties: ${numberOfCategories}`);
+
+
 
